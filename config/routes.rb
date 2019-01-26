e67c61a68407f0fb211571b4f5bc27f4578482bd
Davi Torres <torresdavi04@gmail.com>
2019-01-26 21:07:47 -0200

    alteração

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
   resources :tables
   resources :products
   resources :restaurants
-
-  root ''
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
