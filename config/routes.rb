f81c5d19ac459a9b10d1477efeb21597b724bb7a
arturhbs <arturhenrique71@gmail.com>
2019-01-27 15:54:01 -0200

    .

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :products
   resources :restaurants
 
-  root 'products#index'
+  root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
